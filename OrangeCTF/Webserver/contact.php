ee7a666f6b24cb7da32ced719c61d447516be008
Derek Sneddon <dsneddon00@gmail.com>
2023-03-07 18:55:20 -0700

    Added and tested white list and black list input lockpicking

--- OrangeCTF/Webserver/contact.php
+++ OrangeCTF/Webserver/contact.php
@@ -1,23 +1,31 @@
 <?php
 session_start();
 
-$uploadOk = 2;
+$uploadOk = -1;
 
 if (isset($_POST["submit"])){
     $target_dir = "uploads/"; // default file upload directory
-    $target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);
+    $filename = basename($_FILES["fileUpload"]["name"]);
+    $target_file = $target_dir . basename($_FILES["fileUpload"]["name"]);;
     $file_filetype = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-    if ($file_filetype == "php"){
-        $result = "PHP is not forbidden!";
+    // blacklist
+    if ($file_filetype == "php" || $file_filetype == "php2" || $file_filetype == "php3" || $file_filetype == "php4" || $file_filetype == "php5" || $file_filetype == "phtml"){
+        $result = "PHP is forbidden! Nice try hacker!";
         $uploadOk = 0;
+
+    //whitelist
+    } elseif (strpos($target_file . $file_filetype, ".jpg") === false && strpos($target_file . $file_filetype, ".png") === false && strpos($target_file . $file_filetype, ".img") === false && strpos($target_file . $file_filetype, ".jpeg") === false) {
+        $result = "Please upload only images";
+        $uploadOk = 0;
+
     } else {
         if (file_exists($target_file)){
-            $result = "File already exists!";
+            $result = "{$filename} already exists!";
             $uploadOk = 0;
         }
         if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
-            $result = "File has been successfully uploaded!";
+            $result = "{$filename} has been successfully uploaded!";
             $uploadOk = 1;
         } else {
             $result = "Error uploading file!";
@@ -45,6 +53,18 @@ if (isset($_POST["submit"])){
                 <h3>Upload image:</h3>
                 <input type="file" name="fileUpload" class="fileUpload">
                 <input type="submit" value="Submit" name="submit">    
+
+                <!-- error responses -->
+                <?php
+                  if ($uploadOk == 1){
+
+                    echo "<p class='success'>{$result}</p>";
+
+                  } elseif ($uploadOk == 0){
+                    echo "<p class='error'>{$result}</p>";
+                  }
+                  ?>
+
             </form>
         </div>
     </div>
