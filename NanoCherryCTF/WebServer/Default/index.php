048ccead4c69541f62005f8afd7b0735bb37e29b
Derek Sneddon <dsneddon00@gmail.com>
2023-04-03 12:53:18 -0700

    Default home page complete

--- NanoCherryCTF/WebServer/Default/index.php
+++ NanoCherryCTF/WebServer/Default/index.php
@@ -1,7 +1,295 @@
+<!DOCTYPE html>
 <html>
   <head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Cherry on Top Ice Cream Shop</title>
+    <link rel="stylesheet" href="themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
   </head>
-  <body> <h1>Success!</h1>
-  </body>
+  <body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+    
+    <!-- Page Navigation -->
+    <nav class="navbar custom-navbar navbar-expand-lg navbar-dark" data-spy="affix" data-offset-top="20">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="images/logo.png">
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#home">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#portfolio">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#team">Team</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#testimonial">Testimonial</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Of Page Navigation -->
+
+    <!-- Page Header -->
+    <header class="header">
+        <div class="overlay">
+            <h6 class="subtitle">Cherry on Top</h6>
+            <h1 class="title">Ice Cream Shop</h1>            
+        </div>      
+    </header>
+    <!-- End Of Page Header -->
+
+    <!-- About Section -->
+    <section id="about">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-md-5 col-lg-4">
+                    <img src="images/about.jpg"  class="w-100 img-thumbnail mb-3">
+                </div>
+                <div class="col-md-7 col-lg-8">
+                    <h6 class="section-subtitle mb-0">Making the tastiest ice cream</h6>
+                    <h6 class="section-title mb-3">and nothing else</h6>
+                    <p>Come by the Cherry on Top Icecream Shop for a tasty treat! Try our signature flavors, such as GNU Nanoberry, Eternal Blue Ice, or even Heartbleed Cherry!</p>
+                    <p>With so many different flavors, even your most picky Arch Linux users can enjoy something new!</p>
+                    <p><i>NO REFUNDS AND STOP STICKING YOUR FINGERS IN THE TUBS MOLLY</i></p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End of About Section -->
+
+    <!-- Fatcs Section -->
+    <section class="has-bg-img bg-img-2">
+    <div class="container text-center">
+        <h6 class="section-subtitle">We Are Awesome</h6>
+        <h6 class="section-title mb-6">Some Fun Fucts</h6>
+            <div class="widget-2">
+                <div class="widget-item">
+                    <i class="ti-cup"></i>
+                    <h6 class="title">2</h6>
+                    <div class="subtitle">Most Mid Ice Cream Awards</div>
+                </div>
+                <div class="widget-item">
+                    <i class="ti-face-smile"></i>
+                    <h6 class="title">20+</h6>
+                    <div class="subtitle">Happy Customers</div>
+                </div>
+                <div class="widget-item">
+                    <i class="ti-money"></i>
+                    <h6 class="title">$2M+</h6>
+                    <div class="subtitle">In debt</div>
+                </div>
+            </div>
+    </div>
+    </section>
+
+    <!-- Portfolio section -->
+    <section id="portfolio">
+        <div class="container text-center">
+            <h6 class="section-subtitle">Our Awesome Works</h6>
+            <h6 class="section-title mb-5">Our Portfolio</h6>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="img-wrapper">
+                        <img src="images/ice-cream1.PNG" >
+                        <div class="overlay">
+                            <div class="overlay-infos">
+                                <h5>Cream n Cup</h5>
+                            </div>  
+                        </div>
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="images/ice-cream2.jpg" >
+                        <div class="overlay">
+                            <div class="overlay-infos">
+                                <h5>Bowl with Bananas</h5>
+                            </div>                              
+                        </div>
+                    </div>                  
+                </div>
+                <div class="col-sm-4">
+                    <div class="img-wrapper">
+                        <img src="images/ice-cream3.jpeg" >
+                        <div class="overlay">
+                            <div class="overlay-infos">
+                                <h5>Stick n Cup</h5>
+                            </div>  
+                        </div>
+                    </div>
+                    <div class="img-wrapper">
+                        <img src="images/ice-cream4.jpeg" >
+                        <div class="overlay">
+                            <div class="overlay-infos">
+                                <h5>Strawberry Suprise</h5>
+                            </div>                              
+                        </div>
+                    </div>                  
+                </div>
+                <div class="col-sm-4">
+                    <div class="img-wrapper">
+                        <img src="images/ice-cream5.jpeg" >
+                        <div class="overlay">
+                            <div class="overlay-infos">
+                                <h5>Chocolate Mountain</h5>
+                            </div>  
+                        </div>
+                    </div>                  
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End of portfolio section -->
+
+
+    <!-- Video Section -->
+    <section class="has-bg-img py-lg">
+        <div class="container text-center">
+
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-outline-primary play-control" data-toggle="modal" data-target="#exampleModalCenter">
+              <i class="ti-control-play" ></i>
+            </button>
+            <h6 class="section-title mt-4">See Our Intro Video</h6>
+
+        </div>
+    </section>
+    <!-- End of Video Section -->
+
+    <!-- Team Section -->
+    <section id="team">
+        <div class="container">
+            <h6 class="section-title mb-5 text-center">Meet the Creamy Crew</h6>
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    <div class="card text-center mb-4">
+                        <img class="card-img-top inset" src="images/chad-cherry.png">
+                        <div class="card-body">
+                            <h6 class="small text-primary font-weight-bold">CEO and Founder</h6>
+                            <h5 class="card-title">Chad Cherry</h5>
+                            <p>A big thumbs up to Chad Cherry for founding the Cherry on Top Ice Cream Shop! Loves ice cream, his favorite text editor nano, and putting cherries on random passer-bys' heads!</p>
+                            <div class="socials">
+                                <a href="javascript:void(0)"><i class="ti-facebook"></i> </a>
+                                <a href="javascript:void(0)"><i class="ti-github"></i></a>
+                                <a href="javascript:void(0)"><i class="ti-dropbox"></i></a>
+                                <a href="javascript:void(0)"><i class="ti-twitter"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="card text-center mb-4">
+                        <img class="card-img-top inset" src="images/molly-milk.png">
+                        <div class="card-body">
+                            <h6 class="small text-primary font-weight-bold">Queen of Marketing</h6>
+                            <h5 class="card-title">Molly Milk</h5>
+                            <p>Molly Milk likes chocolate, strawberry, and oreo milk. She, however, frowns upon eggnog.</p>
+                            <div class="socials">
+                                <a href="javascript:void(0)"><i class="ti-facebook"></i> </a>
+                                <a href="javascript:void(0)"><i class="ti-github"></i></a>
+                                <a href="javascript:void(0)"><i class="ti-dropbox"></i></a>
+                                <a href="javascript:void(0)"><i class="ti-twitter"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="card text-center mb-4">
+                        <img class="card-img-top inset" src="images/sam-sprinkles.png">
+                        <div class="card-body">
+                            <h6 class="small text-primary font-weight-bold">Web Developer</h6>
+                            <h5 class="card-title">Sam Sprinkles</h5>
+                            <p>Loves long walks on the beach and assaulting vim users.</p>
+                            <div class="socials">
+                                <a href="javascript:void(0)"><i class="ti-facebook"></i> </a>
+                                <a href="javascript:void(0)"><i class="ti-github"></i></a>
+                                <a href="javascript:void(0)"><i class="ti-dropbox"></i></a>
+                                <a href="javascript:void(0)"><i class="ti-twitter"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    <div class="card text-center mb-4">
+                        <img class="card-img-top inset" src="images/bob-boba.jpg">
+                        <div class="card-body">
+                            <h6 class="small text-primary font-weight-bold">Head of Finances</h6>
+                            <h5 class="card-title">Bob Boba</h5>
+                            <p>Loves helping people manage their money and totally not extorting them.</p>
+                            <div class="socials">
+                                <a href="javascript:void(0)"><i class="ti-facebook"></i> </a>
+                                <a href="javascript:void(0)"><i class="ti-github"></i></a>
+                                <a href="javascript:void(0)"><i class="ti-dropbox"></i></a>
+                                <a href="javascript:void(0)"><i class="ti-twitter"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- End of Team Sectoin -->
+
+    <!-- Testimonial Section -->
+    <section id="testimonial">
+        <div class="container">
+            <h6 class="section-title text-center mb-6">Listen to a Happy Customer!</h6>
+                    <div class="testimonial-wrapper">
+                        <div class="img-holder">
+                            <img src="images/TobyToastProfilePicture.png">                     
+                        </div>
+                        <div class="body">
+                            <p class="subtitle">TRUST ME, THESE GUYS ARE NO GOOD!! Now I'm homeless because my website got hacked into and Bob Boba is trying to collect my debts! DO NOT BUY THEIR CRUSTY ICECREAM!!</p>
+                            <h6 class="title">Toby Toast</h6>
+                        </div>
+                    </div>
+        </div>
+    </section>
+    <!-- End of Testimonial Section -->
+
+    <section class="has-bg-img py-0">
+        <div class="container">
+            <div class="footer">
+                <div class="footer-lists">
+                    <ul class="list">
+                        <li class="list-head">
+                            <h6 class="font-weight-bold">CONTACT INFO</h6>
+                        </li>
+                        <li class="list-body">
+                            <p>Reach out to us on our fake social media!</p>
+                            <p><i class="ti-location-pin"></i>54321 Ice Cream Ave. 8080</p>
+                            <div class="social-links">
+                                <a href="javascript:void(0)" class="link"><i class="ti-facebook"></i></a>
+                                <a href="javascript:void(0)" class="link"><i class="ti-twitter-alt"></i></a>
+                                <a href="javascript:void(0)" class="link"><i class="ti-google"></i></a>
+                                <a href="javascript:void(0)" class="link"><i class="ti-instagram"></i></a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>    
+        </div>
+    </section>
+
+    <!-- scripts -->
+    <script src="jquery/jquery-3.4.1.js"></script>
+    <script src="bootstrap/bootstrap.bundle.js"></script>
+    <script src="bootstrap/bootstrap.affix.js"></script>
+    <script src="js/script.js"></script>
+
+</body>
 </html>
